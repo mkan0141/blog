b71dea9499282bff190e41ef0817d7239d8f5712
mkan0141 <kanzume0141@gmail.com>
2021-01-11 20:36:13 +0900

    setup: contentで記事を表示できるようになった:tada:

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+    <nuxt-content :document='article'></nuxt-content>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+    return { article }
+  }
+})
+</script>
