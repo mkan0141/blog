7d7ead8c9d91e82128fdbe3a6b821f1472b47024
mkan0141 <kanzume0141@gmail.com>
2021-01-13 08:10:46 +0900

    #8 Fix js,vue lint warnings

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,19 +1,17 @@
 <template>
   <article>
-    <nuxt-content :document='article'></nuxt-content>
+    <nuxt-content :document="article"></nuxt-content>
   </article>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import axios from 'axios'
-
 
 export default Vue.extend({
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
     return { article }
-  }
+  },
 })
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   plugins: [
-    plugin(function({ addBase, theme }) {
+    plugin(function ({ addBase, theme }) {
       addBase({
-        'h1': { fontSize: theme('fontSize.3xl') },
-        'h2': { fontSize: theme('fontSize.2xl') },
-        'h3': { fontSize: theme('fontSize.xl') },
+        h1: { fontSize: theme('fontSize.3xl') },
+        h2: { fontSize: theme('fontSize.2xl') },
+        h3: { fontSize: theme('fontSize.xl') },
       })
-    })
-  ]
+    }),
+  ],
 }
