0d1d6ced8574dfe111d9404757d9340d23e7790b
mkan0141 <kanzume0141@gmail.com>
2021-01-12 23:34:34 +0900

    #8 CodeBlockのスタイル追加

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -16,3 +16,23 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang="scss">
+$code-block-bg-color: #2f495e;
+
+.nuxt-content-highlight {
+  background-color: $code-block-bg-color;
+
+  .filename {
+    position: absolute;
+    padding: 0.05em 0.5em;
+    color: white;
+    background-color: lighten($code-block-bg-color, 15%);
+    z-index: 1;
+  }
+
+  pre {
+    padding: 1.8em 1em;
+  }
+}
+</style>
