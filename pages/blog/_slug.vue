7273fd8f6ec7f962716815438e0bafb081e30682
mkan0141 <kanzume0141@gmail.com>
2021-01-16 19:36:57 +0900

    #13 markdownをいい感じのスタイルで表示させるようにした

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -7,8 +7,12 @@
         </article>
       </main>
       <aside class="col-span-1">
-        <div>
-          My Name is neko neko neko neko neko nkeo;
+        <div class="toc">
+          <ul>
+            <li v-for="{ id, text, depth } in article.toc" :key="id">
+              <a :href="'#' + id" :class="'toc-h' + depth">{{ text }}</a>
+            </li>
+          </ul>
         </div>
       </aside>
     </div>
@@ -28,20 +32,57 @@ export default Vue.extend({
 
 <style lang="scss">
 $code-block-bg-color: #2f495e;
+$bg-gray-100: #f7fafc;
+$bg-gray-300: #e2e8f0;
+$bg-blue-100: #ebf8ff;
+$bg-blue-300: #90cdf4;
 
-.nuxt-content-highlight {
-  background-color: $code-block-bg-color;
-
-  .filename {
-    position: absolute;
-    padding: 0.05em 0.5em;
-    color: white;
-    background-color: lighten($code-block-bg-color, 15%);
-    z-index: 1;
+.nuxt-content {
+  h2, h3 {
+    margin-bottom: 1.6rem;
+    padding: 0.2rem 0;
+    border-bottom: 1px solid $bg-gray-300;
   }
 
-  pre {
-    padding: 1.8em 1em;
+  p {
+    margin-bottom: 1rem;
+
+    code {
+      margin: 0 0.25rem;
+      padding: 0.15rem 0.25rem;
+      background-color: $bg-gray-100;
+      border: 1px solid $bg-gray-300;
+      border-radius: 2px; 
+    }
+  }
+
+  blockquote {
+    margin-bottom: 2rem;
+    padding: 1rem;
+    background-color: $bg-blue-100;
+    border-left: 5px solid $bg-blue-300;
+    
+    p {
+      padding: 0;
+    }
+  }
+
+  .nuxt-content-highlight {
+    margin-bottom: 2rem;
+    background-color: $code-block-bg-color;
+    border-radius: 5px; 
+    
+    .filename {
+      position: absolute;
+      padding: 0.05em 0.5em;
+      color: white;
+      background-color: lighten($code-block-bg-color, 15%);
+      z-index: 1;
+    }
+
+    pre {
+      padding: 1.8em 1em;
+    }
   }
 }
 </style>
