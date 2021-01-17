1689e1a25d787616be704e3f159fd20de8c3d497
mkan0141 <kanzume0141@gmail.com>
2021-01-17 16:42:11 +0900

    #13 add Header

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="bg-white mb-12">
+    <div class="flex items-center justify-between h-16 mx-auto max-w-4xl">
+      <span class="text-2xl font-bold text-orange-600">mikan is oishiii</span>
+    </div>
+  </header>
+</template>
