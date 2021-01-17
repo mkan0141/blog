cf0744c1155ba27a52de97ec0aea70d1e2866dc2
mkan0141 <kanzume0141@gmail.com>
2021-01-17 23:16:21 +0900

    #13 1カラムレイアウトにした

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="max-w-5xl mx-auto px-4 sm:px-6 xl:max-w-6xl xl:px-0">
-    <div class="grid grid-cols-4">
-      <main class="col-span-3">
-        <article class="break-words xl:px-5 xl:py-1">
+  <div class="max-w-4xl mx-auto px-4 sm:px-6 xl:max-w-4xl xl:px-0">
+      <main class="">
+        <article class="break-words xl:mx-5 xl:my-1">
+          <div class="mb-8">
+            <h1 class="">{{ article.title }}</h1>
+            <ul class="tags"><li class="tag inline px-2 py-1 bg-orange-500 text-white rounded" v-for="tag in article.tags" :key="tag">{{ tag }}</li></ul>
+          </div>
           <nuxt-content :document="article"></nuxt-content>
         </article>
       </main>
-      <aside class="col-span-1">
-        <div class="toc">
-          <ul>
-            <li v-for="{ id, text, depth } in article.toc" :key="id">
-              <a :href="'#' + id" :class="'toc-h' + depth">{{ text }}</a>
-            </li>
-          </ul>
-        </div>
-      </aside>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+interface Article {
+  title: string,
+  img: String,
+  path: String,
+  createdAt: String,
+}
+
 export default Vue.extend({
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
+    console.log(article)
     return { article }
   },
+  data() {
+    return {
+      recent_articles: [] as any,
+      categories: [] as any,
+    }
+  },
+  async created() {
+    this.recent_articles = await this.$content('articles')
+      .only(['title', 'img', 'path', 'createdAt'])
+      .sortBy('createdAt', 'desc')
+      .limit(5)
+      .fetch();
+    this.categories = await this.$content('articles')
+      .only(['category'])
+      .fetch()
+    console.log(this.categories)
+    console.log(this.recent_articles)
+  }
 })
 </script>
 
@@ -37,7 +56,20 @@ $bg-gray-300: #e2e8f0;
 $bg-blue-100: #ebf8ff;
 $bg-blue-300: #90cdf4;
 
+.tags {
+  .tag:nth-child(n+2) {
+    margin-left: 0.5rem;
+  }
+}
+
 .nuxt-content {
+  background-color: white;
+  border-radius: 10px;
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
   h2, h3 {
     margin-bottom: 1.6rem;
     padding: 0.2rem 0;
@@ -63,7 +95,7 @@ $bg-blue-300: #90cdf4;
     border-left: 5px solid $bg-blue-300;
     
     p {
-      padding: 0;
+      margin-bottom: 0;
     }
   }
 
